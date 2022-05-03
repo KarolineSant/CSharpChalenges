e8a9513e342a56f66b885bb4c6c7f68758351cdb
unknown <karolinesantos1296@gmail.com>
2022-05-03 20:35:14 -0300

    my first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Exercicio1
+{
+    class Exercicio1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Digite 5 números:");
+            decimal a = decimal.Parse(Console.ReadLine());
+            decimal b = decimal.Parse(Console.ReadLine());
+            decimal c = decimal.Parse(Console.ReadLine());
+            decimal d = decimal.Parse(Console.ReadLine());
+            decimal e = decimal.Parse(Console.ReadLine());
+
+            decimal[] numeros = { a, b, c, d, e };
+            decimal maiorNumero = numeros.Max();
+            decimal menorNumero = numeros.Min();
+            decimal diferenca = maiorNumero - menorNumero;
+
+            Console.WriteLine("O maior número é {0}", maiorNumero);
+            Console.WriteLine("A diferença entre o maior número e o menor número é {0}", diferenca);
+
+        }
+
+    }
+}
